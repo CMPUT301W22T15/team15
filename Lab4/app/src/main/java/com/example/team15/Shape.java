dd8bc31af74943c2a1a853d4d3399ff6247d4323
TruongGiangPham-1 <66976914+TruongGiangPham-1@users.noreply.github.com>
2022-02-01 19:15:43 -0700

    Update Shape.java

--- Lab4/app/src/main/java/com/example/team15/Shape.java
+++ Lab4/app/src/main/java/com/example/team15/Shape.java
@@ -3,7 +3,7 @@ package com.example.team15;
 public class Shape {
     public int x;
     public int y;
-    public String color = "$blue";
+    public String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
